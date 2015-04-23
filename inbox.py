58a89daea6c5dcf32bcaf941c1f4447581b437f1
Arkiver2 <Arkiver@hotmail.com>
2015-04-23 10:43:27 +0200

    inbox.py: fix

--- inbox.py
+++ inbox.py
@@ -37,8 +37,7 @@ class InboxServer(smtpd.SMTPServer, object):
                 elif mailpart[0] == "text/plain":
                     mailplain = mailpart[1]
         
-        log.debug(dict(rawdata=data, to = rcpttos, sender = mailfrom, subject = subject, body = mailcontent, attachments = attachments))
-        log.debug(dict(rawdata=data, to = rcpttos, sender = mailfrom, subject = subject, body = mailcontent, attachments = attachments))
+        log.debug(dict(rawdata=data, to = rcpttos, sender = mailfrom, subject = subject, mailtext = mailtext, mailplain = mailplain, attachments = attachments))
         return self._handler(rawdata=data, to = rcpttos, sender = mailfrom, subject = subject, mailtext = mailtext, mailplain = mailplain, attachments = attachments)
         
     def parse_subject(self, subject):
